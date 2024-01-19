b40d93570346e60b482d8f27d556bd1fc44eb216
Inoyuuuuu <inoyuuuuu@gmail.com>
2024-01-19 21:00:28 +0100

    fix error multple access thing

--- lcBoomboxVolumeControl/lcBoomboxVolumeControl/BoomboxVolumeControlMod.cs
+++ lcBoomboxVolumeControl/lcBoomboxVolumeControl/BoomboxVolumeControlMod.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using lcBoomboxVolumeControl.Patches;
+using lcBoomboxVolumeControl;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,34 @@ using System.Threading.Tasks;
 
 namespace lcBoomboxVolumeControl
 {
-    public class BoomboxVolumeControlMod
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class BoomboxVolumeControlMod : BaseUnityPlugin
     {
+        private const string modGUID = "inoyu.lcBoomboxVolumeControl";
+        private const string modName = "BoomboxVolumeControl";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static BoomboxVolumeControlMod instance;
+
+        internal ManualLogSource mls;
+
+        public static BoomboxVolumeDownOnButtonPress InputActionInstance = new BoomboxVolumeDownOnButtonPress();
+
+        void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("mod awake");
+
+            harmony.PatchAll(typeof(BoomboxVolumeControlMod));
+          
+        }
     }
 }
---
+++ lcBoomboxVolumeControl/lcBoomboxVolumeControl/BoomboxVolumeDownOnButtonPress.cs
@@ -0,0 +1,16 @@
+﻿using LethalCompanyInputUtils.Api;
+using UnityEngine.InputSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lcBoomboxVolumeControl.Patches
+{
+    public class BoomboxVolumeDownOnButtonPress : LcInputActions
+    {
+        [InputAction("<Keyboard>/o", Name = "BoomboxVolDown", GamepadPath = "<Gamepad>/Button North", ActionType = InputActionType.Button)]
+        public InputAction BoomboxVolDownKey {  get; set; }
+    }
+}
